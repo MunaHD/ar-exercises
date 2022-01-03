35d35db037a5da7396a5ddaa160ae16ba0a5483c
Muna Dahir <muna.dahir@hotmail.com>
2022-01-03 16:36:06 -0500

    correct @store2 id

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,7 +6,7 @@ puts "----------"
 
 # Your code goes here 
 @store1 = Store.first
-@store2 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
 
 @store1.update(name: "Walmart") 
-puts @store1.name
+//puts @store1.name
