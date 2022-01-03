0dacd578f3c90a65c91f67436f5b72e03ef0c2ab
Muna Dahir <muna.dahir@hotmail.com>
2022-01-03 16:28:59 -0500

    finish ex. 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes here 
+@store1 = Store.first
+@store2 = Store.find_by(id: 1)
+
+@store1.name = 'Walmart'
+# puts @store1.name
