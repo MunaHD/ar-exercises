b38b00c56d4a30f11882354a8fc6f4a9e7908a7a
Muna Dahir <muna.dahir@hotmail.com>
2022-01-03 20:19:38 -0500

    complete ex. 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,11 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, inclusion: { in: 40..200 }
   validates :store_id, presence: true
+
+  after_save :add_employee_password
+
+  private
+  def add_employee_password
+    self.password = [*("A".."Z")].sample(8).join
+  end
 end
